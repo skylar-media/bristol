ad8bccf1e11255c63a53a2ba4eccf6913c1f9268
Karan Mahajan <karanmahajan321@gmail.com>
2026-01-09 15:14:50 -0500

    Update phone number

--- lib/data/branches.ts
+++ lib/data/branches.ts
@@ -13,7 +13,7 @@ export const locations: Location[] = [
     addressLine1: "6 Tracey Blvd.",
     addressLine2: "Brampton, ON, L6T 5R9",
     phone: "(647) 584-9852",
-    phoneHref: "9054538080",
+    phoneHref: "6475849852",
     id: "btn-bristol-lp-branch-brampton-tracy-call",
   },
   {
@@ -21,7 +21,7 @@ export const locations: Location[] = [
     addressLine1: "216 Rutherford Road South",
     addressLine2: "Brampton, ON, L6W 3J6",
     phone: "(647) 362-1965",
-    phoneHref: "9057908086",
+    phoneHref: "6473621965",
     id: "btn-bristol-lp-branch-brampton-bramalea-call",
   },
   {
@@ -29,7 +29,7 @@ export const locations: Location[] = [
     addressLine1: "305 Steeles Avenue East.",
     addressLine2: "Milton, ON, L9T 1Y2",
     phone: "(647) 693-2452",
-    phoneHref: "9058649880",
+    phoneHref: "6476932452",
     id: "btn-bristol-lp-branch-milton-call",
   },
   {
@@ -37,7 +37,7 @@ export const locations: Location[] = [
     addressLine1: "2175 Royal Windsor Drive",
     addressLine2: "Mississauga, ON, L5J 1K6",
     phone: "(647) 931-1763",
-    phoneHref: "9058238499",
+    phoneHref: "6479311763",
     id: "btn-bristol-lp-branch-mississauga-call",
   },
 ];
