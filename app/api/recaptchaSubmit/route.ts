72f55bc4e8f1c86b0d390d068ac845e7885b86e2
Karan Mahajan <karanmahajan321@gmail.com>
2026-01-09 22:38:32 -0500

    Fix recaptcha endpoint

--- app/api/recaptchaSubmit/route.ts
+++ app/api/recaptchaSubmit/route.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request, response: Response) {
+export async function POST(request: Request) {
   const secretKey = process.env.RECAPTCHA_SECRET_KEY;
 
   const postData = await request.json();
